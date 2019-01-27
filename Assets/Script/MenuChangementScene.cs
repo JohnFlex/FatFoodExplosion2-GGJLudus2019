b69ef9f7dc0927c589182d6d9e6f86639165b548
John Flex <marcelih55@gmail.com>
2019-01-27 13:39:49 +0100

    Adding Main Menu

---
+++ Assets/Script/MenuChangementScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuChangementScene : MonoBehaviour
+{
+    public void changeMenuScene(string nameScene)
+    {
+        Application.LoadLevel(nameScene);
+    }
+
+    public void quitteApplication()
+    {
+        Application.Quit();
+        Debug.Log("On quitte l'application !");
+    }
+}
