6289a4ef91fecda58ebb4055d54bf2e93d9701cb
Midonk <t_finet@hotmail.fr>
2019-01-27 15:14:24 +0100

    timer de jeu + victory script

---
+++ Assets/Script/Victory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    public charBehaviour charScript;
+    public UIScript timer;
+    private int ennemies;
+    private GameObject[] boufs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boufs = GameObject.FindGameObjectsWithTag("bouf");
+        ennemies = boufs.Length;
+    }
+
+    void Update()
+    {
+        if(charScript.poid == ennemies)
+        {
+            timer.SendMessage("win");
+        }
+    }
+}
